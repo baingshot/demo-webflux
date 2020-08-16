e2e78986659aef4ceb01ae141976d736b89c7bb4
Dmitrii Khlopov <baingshot@gmail.com>
2020-08-16 17:51:52 +0300

    Add Swagger

--- src/main/java/ru/eda/demowebflux/controller/InfoController.java
+++ src/main/java/ru/eda/demowebflux/controller/InfoController.java
@@ -1,13 +1,15 @@
 package ru.eda.demowebflux.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
 
+    @ApiOperation(value = "Get backend status state")
     @GetMapping("/status")
     public String getStatus(){
-        return "";
+        return "OK";
     }
 }
